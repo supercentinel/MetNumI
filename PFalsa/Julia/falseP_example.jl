274a4a14a304b9fd4d9dc2bbd80714feb3edc9a5
4L4N <digitalforce.ac@gmail.com>
2023-09-07 13:38:33 -0600

    added plot

--- PFalsa/Julia/falseP_example.jl
+++ PFalsa/Julia/falseP_example.jl
@@ -1,7 +1,23 @@
+using Plots
+
 include("pfalsa.jl")
 
 function f(x::Float64)::Float64
     return x^3 + x^2 - 4x - 1
 end
 
-println(falseP(f, 1.0, 2.0, 0.0005, 100))
+results = falseP(f, 1.0, 2.0, 0.0005, 100)
+
+println(results)
+
+
+plot(f, -2, 2, label="f(x) = x^3 + x^2 - 4x - 1", legend=:topleft, show=true)
+ylims!(-2, 2)
+vline!([0], color=:black, line=:dash)
+hline!([0], color=:black, line=:dash)
+scatter!([results.x_2[size(results.x_2, 1)]], [0], label="Root", color=:red)
+
+
+readline()
+
+println()
